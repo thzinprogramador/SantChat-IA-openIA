ed5d41702d664633bbb7bce02630fac5dd18610a
Thzin Programador <107508243+thzinprogramador@users.noreply.github.com>
2025-06-07 19:06:31 -0300

    Create firebase.py

---
+++ firebase.py
@@ -0,0 +1,20 @@
+import firebase_admin
+from firebase_admin import credentials, db
+
+def iniciar_firebase():
+    if not firebase_admin._apps:
+        cred = credentials.Certificate("firebase_credentials.json")
+        firebase_admin.initialize_app(cred, {
+            'databaseURL': "https://SEU_URL.firebaseio.com/"  # 🔁 coloque sua URL aqui!
+        })
+
+def salvar_memoria_firebase(memoria):
+    iniciar_firebase()
+    ref = db.reference("/memoria")
+    ref.set(memoria)
+
+def carregar_memoria_firebase():
+    iniciar_firebase()
+    ref = db.reference("/memoria")
+    dados = ref.get()
+    return dados if dados else []
