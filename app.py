e5abdc15dbffa7e2569537cb3db1f73bd713df1c
Thzin Programador <107508243+thzinprogramador@users.noreply.github.com>
2025-06-08 18:31:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import firebase_admin
 from firebase_admin import credentials, db
 
 # --- Configurações iniciais ---
-st.set_page_config(page_title="SantChat", page_icon="🤖", layout="wide")
+st.set_page_config(page_title="SantChat", page_icon="🤖", layout="centered")
 col1, col2 = st.columns([1, 5])
 
 # --- Firebase Initialization ---
