a3b4d45d927e8962cf528068a55de1fc1172cfa6
Thzin Programador <107508243+thzinprogramador@users.noreply.github.com>
2025-06-08 20:21:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def load_css():
             right: 0;
             height: var(--header-height);
             background: white;
-            box-shadow: 0 2px 10px #002982;
+            box-shadow: 0 2px 10px rgba(0, 52, 122, 1);
             display: flex;
             align-items: center;
             padding: 0 20px;
